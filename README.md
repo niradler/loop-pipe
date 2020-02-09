f3a4c85de36e86d935b5cb4744a5807ff8a93056
Nir Adler <github@niradler.com>
2020-02-09 21:12:28 +0200

    v001

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # loop-pipe
-work in progress
+
+loop over piped data.
+
+# Installation
+
+```
+npm i -g loop-pipe
+```
+
+# Usage
+
+- --verbose (debug mode, add extra logs, default:false)
+- --dry (dry run will print the command and not actually run it, default:false)
+- --pattern (the pattern to create the array, pass to split function, default:\n)
+- --clean (you can transform the data with pattern to clean for not needed character, default:/\r?\n|\r/g)
+
+# Example
+
+```
+echo "a,b,c" | lp --p="," "echo {v} is in location {i}"
+```
---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+const minimist = require("minimist");
+const execa = require("execa");
+
+var _data = "";
+
+function cleanText(arr, pattern) {
+  return arr.map(d => d.replace(pattern, ""));
+}
+
+function loopData(argv, data) {
+  const exec = argv._[0];
+  const debug = argv.verbose || argv.v || false;
+  const dryRun = argv.dry || argv.d || false;
+  const pattern = argv.pattern || argv.p || "\n";
+  const cleanPattern = argv.clean || argv.c || /\r?\n|\r/g;
+  let arr = data.split(pattern);
+  arr = cleanText(arr, cleanPattern);
+  if (debug)
+    console.log({
+      argv,
+      data,
+      arr,
+      options: {
+        debug,
+        dryRun,
+        pattern,
+        cleanPattern
+      }
+    });
+  for (let i = 0; i < arr.length; i++) {
+    const v = arr[i];
+    const cmd = exec.replace(/{i}/g, i).replace(/{v}/, v);
+    if (dryRun || debug) console.log(cmd);
+    else {
+      execa(cmd, [], { cwd: process.cwd() }).stdout.pipe(process.stdout);
+    }
+  }
+}
+
+function withPipe(data) {
+  const argv = minimist(process.argv.slice(2));
+  loopData(argv, data.trim());
+}
+
+function withoutPipe() {
+  console.log("no content was piped");
+}
+
+var self = process.stdin;
+self.on("readable", function() {
+  var chunk = this.read();
+  if (chunk === null) {
+    if (!_data) withoutPipe();
+  } else {
+    _data += chunk;
+  }
+});
+self.on("end", function() {
+  withPipe(_data);
+});
